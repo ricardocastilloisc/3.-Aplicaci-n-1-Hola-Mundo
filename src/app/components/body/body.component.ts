8088ec050e4991239700071057d8ad1e6d96adf6
ricardocastilloisc <ricardocastilloisc@gmail.com>
2018-11-08 16:15:33 -0500

    3.12. Directivas estructurales ngFor y el ngIf

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -6,6 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./body.component.css']
 })
 export class BodyComponent implements OnInit {
+  mostrar = true;
+
+  frase: any =
+  {
+    mensaje: 'Un gran poder requiere una gran responsabilidad',
+    autor: 'Ben Parker'
+  };
+
+  personajes: string [] = [
+    'Spiderman',
+    'Venom',
+    'Dr. Octopus'];
 
   constructor() { }
 
